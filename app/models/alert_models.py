c446e418c877fbf5d25151b7d66823b3280333cc
Gr1l3m <grilem@outlook.com>
2023-01-31 12:59:04 -0600

    feat(db): using alert_type as primary key, removed id

--- app/models/alert_models.py
+++ app/models/alert_models.py
@@ -2,8 +2,8 @@ from typing import Optional
 from sqlmodel import Field, SQLModel, create_engine
 
 class Alert(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    alert_type: str
+    # id: Optional[int] = Field(default=None, primary_key=True)
+    alert_type: str = Field(default=None, primary_key=True)
     schedule: str
 
 
